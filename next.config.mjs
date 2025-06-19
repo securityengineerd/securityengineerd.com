c1fbfb0e3ce56353dfcc5b20eed43acdd0c24301
Joshua Marcum <josh.marcum@icloud.com>
2025-06-19 01:55:57 -0700

    Sync

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const withMDX = mdx({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   sassOptions: {
